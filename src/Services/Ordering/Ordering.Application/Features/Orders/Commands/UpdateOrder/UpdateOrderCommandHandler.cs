d7a3ada8e11724b52f61e40c7d1f57b7e2847b23
Orkhan Babaev <babaev.or@yandex.ru>
2022-11-04 16:21:59 +0300

    little improvments

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
 using Ordering.Domain.Entities;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,20 +18,18 @@ namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
         public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, 
             ILogger<UpdateOrderCommandHandler> logger)
         {
-            _orderRepository = orderRepository;
-            _mapper = mapper;
-            _logger = logger;
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
         {
-            // update order
             var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
 
             if (orderToUpdate == null)
             {
                 _logger.LogError("Order not exist on database.");
-                //throw new NotFoundException(nameof(Order), request.Id);
             }
 
             _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
